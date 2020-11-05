6b8bb1865295095b8bb32ff31686bcb4f468a1e8
EgorKorsunskiy <georgykors@gmail.com>
2020-11-05 20:50:23 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Table } from './tableElement/index.js';
+import { Table } from './TableElement/index.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
--- src/tableElement/index.js
+++ src/tableElement/index.js
@@ -1,21 +1,88 @@
 import React from 'react';
-import { Cell } from '../cellElement/index.js'
+import { Cell } from '../CellElement/index.js'
 import './index.css';
 
+const DEFAULT_CELL_COLOR = '#fff';
+const MAX_HEXADECIMAL_NUMBER = 16777215;
 const CELL_SIZE = 100;
-const WINDOW_HEIGHT = window.innerHeight;
-const WINDOW_WIDTH = window.innerWidth;
 
 export class Table extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            cells:[{
+                key:'',
+                color:''
+            }]
+        }
+      }
+
+    returnCurrentCellElement(key, cellState){
+
+      let thisElement = cellState.find(el => el.key === key) || null;
+      let thisElementIndex = cellState.indexOf(thisElement);
+
+      return {thisElement, thisElementIndex};
+    }
+
+    generateCellColor(key){ 
+        const CELLS = this.state.cells;
+
+        let thisElement = this.returnCurrentCellElement(key, CELLS).thisElement;
+        let thisElementIndex = this.returnCurrentCellElement(key, CELLS).thisElementIndex;
+        let color;
+
+        try{
+           color = thisElement.color
+           color = (color === DEFAULT_CELL_COLOR) ? '#' + Math.floor(Math.random() * MAX_HEXADECIMAL_NUMBER).toString(16) : DEFAULT_CELL_COLOR;
+        }
+        catch(TypeError){
+          color = '#' + Math.floor(Math.random() * MAX_HEXADECIMAL_NUMBER).toString(16);
+        }
+  
+        if(thisElement){
+          this.state.cells.splice(thisElementIndex, 1);
+        }
+
+        this.state.cells.push({
+          key,
+          color
+        })
+
+        this.setState(state => ({
+          cells: state.cells
+        }))
+    }
+
+    getCellStyle(key){
+        const CELL = this.state.cells;
+
+        let thisElement = this.returnCurrentCellElement(key, CELL).thisElement;
+        let color;
+
+        try{
+            color = thisElement.color
+        }
+        catch(TypeError){
+            color = DEFAULT_CELL_COLOR;
+        }
+
+        return {background: color};
+      }
+
     createGrid(){
+      const WINDOW_HEIGHT = window.innerHeight;
+      const WINDOW_WIDTH = window.innerWidth;
+
       let blocks = [];
       let cells = [];
       let key = 0
     
       for(let i=0;i<=WINDOW_HEIGHT-CELL_SIZE;i+=CELL_SIZE){
         for(let j=0;j<=WINDOW_WIDTH;j+=CELL_SIZE){
-          cells.push(<Cell key={key}/>);
+          cells.push(
+          <Cell key={key} style={() => this.getCellStyle(key)} click={() => this.generateCellColor(key)}/>);
           key++;
         }
         blocks.push(<div key={key}>{cells}</div>);
